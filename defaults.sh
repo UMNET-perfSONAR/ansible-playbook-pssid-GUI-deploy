7dd06bfea794c3550d4607020f913eae1dd54fa2
wmarcoyu <wmarcoyu@umich.edu>
2024-06-17 22:06:37 -0400

    Finish implementing the playbook for testing

---
+++ defaults.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directory = "inventory"
+host_group = "pssid-gui-controller"
+deploy_role_name = "ansible-role-pssid-GUI-install-configure"
+
+mkdir -p ${directory}/group_vars
+
+if ! [ -f ${directory}/group_vars/${host_group}/${deploy_role_name}.yml ]; then
+    cp roles/${deploy_role_name}/defaults/main.yml \
+       ${directory}/group_vars/${host_group}/${deploy_role_name}.yml
+fi
